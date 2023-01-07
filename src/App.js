4c5686fac44b9e68edf75e2decd22394d95e1102
udhaymsd <udhaymsd007@gmail.com>
2023-01-07 10:51:00 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Layout from './layout';
+import Login from './login';
+import Navigation from './navigation';
+
+function App() {
+    return ( <
+        Navigation / >
+    );
+}
+
+export default App;
---
+++ src/navigation.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Layout from './layout';
+import Login from './login';
+import Tickets from './tickets';
+import Signup from './signup';
+import ProtectedRoute from './protectedRoute';
+import Customer from './customer'
+import CreateTicket from './createTicket';
+//import TicketContext from './context/TicketContext';
+import Agents from './components/AgentPage/agents';
+
+
+const Navigation = () =>{
+
+return (
+    <div>      
+        <Router>
+        <div>
+            <Switch>
+                {/* <TicketContext.Provider value={ticketContext.ticketData}> */}
+                    <Route path="/" exact component={Login}></Route>
+                    <Route path="/createTicket" exact component={CreateTicket}></Route>
+                    <ProtectedRoute path="/tickets" exact component={Tickets}></ProtectedRoute>
+                    <Route path="/register" exact component={Signup}></Route>
+                    <ProtectedRoute path="/layout" exact  component={Layout} ></ProtectedRoute>
+                    <ProtectedRoute path="/customer" exact component={Customer}></ProtectedRoute>
+                    <ProtectedRoute path="/agents" exact  component={Agents}></ProtectedRoute>
+                {/* </TicketContext.Provider> */}
+            </Switch>
+        </div>
+    </Router>
+    </div>
+    
+)
+}
+
+export default Navigation;
